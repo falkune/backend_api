21d21a37ff6007c20af2964105ed40d783e6a6d8
falkune <iboudiokel@gmail.com>
2024-07-12 16:14:05 +0200

    update

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 require_once "controllers/UserController.php";
 require_once "controllers/TaskController.php";
@@ -9,27 +12,30 @@ $url = isset($_GET['url']) ? $_GET['url'] : "home";
 switch($url){
     case "register":
         if($_SERVER['REQUEST_METHOD'] == "POST"){
-            $nom      = $_POST['nom'];
-            $prenom   = $_POST['prenom'];
-            $email    = $_POST['email'];
-            $password = $_POST['password'];
+            $data = json_decode(file_get_contents("php://input"), true);
+            $nom      = $data['nom'];
+            $prenom   = $data['prenom'];
+            $email    = $data['email'];
+            $password = $data['password'];
             $user = new UserController($nom, $prenom, $email, $password);
             $user->register();
         }
         break;
     case "login":
         if($_SERVER['REQUEST_METHOD'] == "POST"){
-            $email    = $_POST['email'];
-            $password = $_POST['password'];
+            $data = json_decode(file_get_contents("php://input"), true);
+            $email    = $data['email'];
+            $password = $data['password'];
             UserController::login($email, $password);
         }
         break;
     case "add_task":
         if($_SERVER['REQUEST_METHOD'] == "POST"){
-            $taskName    = $_POST['nom'];
-            $description = $_POST['description'];
-            $endDate     = $_POST['date'];
-            $userId      = $_POST['id'];
+            $data = json_decode(file_get_contents("php://input"), true);
+            $taskName    = $data['nom'];
+            $description = $data['description'];
+            $endDate     = $data['date'];
+            $userId      = $data['id'];
             TaskController::addTask($taskName, $description, $endDate, $userId);
         }
         break;
